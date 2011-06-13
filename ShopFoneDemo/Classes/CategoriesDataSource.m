ae872d1769e71a64380eaf3183f065edc586a5e8
Matthew Sacks <matthew@matthewsacks.com>
2011-06-13 12:25:14 -0700

    updated copyright notices and license

--- ShopFoneDemo/Classes/CategoriesDataSource.m
+++ ShopFoneDemo/Classes/CategoriesDataSource.m
@@ -2,9 +2,21 @@
 //  CategoriesDataSource.m
 //  ShopFoneDemo
 //
-//  Created by PJ Cabrera on 10/28/10.
-//  Copyright 2010 PJ Cabrera. All rights reserved.
-//
+/*
+ Copyright 2011 GlassCode Corporation www.glasscodeinc.com
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import "CategoriesDataSource.h"
 #import "CategoriesItem.h"
